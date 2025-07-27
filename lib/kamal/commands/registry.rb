2a6cccdaf44b34a4ddf4e53bc30272285d96624f
Aleksey Leshchuk <leshchuk@gmail.com>
2025-07-27 20:18:07 +0300

    Update registry.rb
    commented out registry login / logout ( it will be done via docker config itself )

--- lib/kamal/commands/registry.rb
+++ lib/kamal/commands/registry.rb
@@ -2,15 +2,17 @@ class Kamal::Commands::Registry < Kamal::Commands::Base
   def login(registry_config: nil)
     registry_config ||= config.registry
 
-    docker :login,
-      registry_config.server,
-      "-u", sensitive(Kamal::Utils.escape_shell_value(registry_config.username)),
-      "-p", sensitive(Kamal::Utils.escape_shell_value(registry_config.password))
+    # cr.yandex registry will be configred manually, via YC, so no login will be needed
+    # docker :login,
+    #   registry_config.server,
+    #   "-u", sensitive(Kamal::Utils.escape_shell_value(registry_config.username)),
+    #   "-p", sensitive(Kamal::Utils.escape_shell_value(registry_config.password))
   end
 
   def logout(registry_config: nil)
     registry_config ||= config.registry
-
-    docker :logout, registry_config.server
+      
+    # same here
+    # docker :logout, registry_config.server
   end
 end
